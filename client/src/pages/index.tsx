421757120826afce83de02a964863e4064191080
Gilson Santos <gilsonjosert@gmail.com>
2021-02-25 22:21:58 +0000

    HomePage

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,25 +1,5 @@
-import Head from 'next/head'
+import Main from "components/Main"
 
-// import styles from '../styles/Home.module.css' \\ className={styles.container}
-
-type Props = {
-    title:string
-}
-
-export default function Home({title = "React Avançado!"}:Props) {
-  return (
-    <>
-      <Head >
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header>
-          <h1>Bem-vindo a plataforma de games.</h1>
-          <p>Aqui sera uma listagem de conteúdo!</p>
-      </header>
-      <main>
-        Listagem aqui...
-      </main>
-    </>
-  )
+export default function Home() {
+    return <Main />
 }
